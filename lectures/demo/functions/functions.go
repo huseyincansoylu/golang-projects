c598f982956926a96e7fa0b6bc7f2cf90d6f9188
huseyin <huseyin@keyko.io>
2022-07-01 02:23:40 +0300

    functions in go

--- lectures/demo/functions/functions.go
+++ lectures/demo/functions/functions.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
+func double(x int) int {
+	return x + x
+}
 
+func add(a, b int) int {
+	return a + b
+}
+
+func greet() {
+	fmt.Println("Hello from greet func!")
+}
+
+func main() {
+    greet()
+
+	dozen := double(6)
+
+	fmt.Println("A dozen is = " , dozen)
+
+	bakersDozen := add(dozen , 1)
+
+	fmt.Println("A bakersDozen is = " , bakersDozen)
+
+	anotherBakersDozen := add(double(6), 9)
+	fmt.Println("A anotherBakersDozen is = " , anotherBakersDozen)
 }
