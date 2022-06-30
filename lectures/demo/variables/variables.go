08078c25d1043ec0f85fccfa8965be7465086afc
huseyin <huseyin@keyko.io>
2022-07-01 01:46:50 +0300

    variables in go

--- lectures/demo/variables/variables.go
+++ lectures/demo/variables/variables.go
@@ -3,4 +3,42 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Huseyin"
+	fmt.Println("my name is",myName)
+
+	var name string = "Karen"
+	fmt.Println("name =", name)
+
+    username := "admin"
+	fmt.Println("username=", username)
+
+    var sum int
+	fmt.Println("The sum is=", sum) //default 0
+
+	part1, other := 1, 5
+	fmt.Println("part1=", part1, "other is=", other)
+     
+	//other will be  5
+	
+	part2, other := 2, 0
+	fmt.Println("part2=", part2, "other is=", other)
+
+	//other will be  0
+
+
+	sum = part1 + part2
+	fmt.Println("sum=", sum)
+
+	var(
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+
+	fmt.Println(lessonName)
+	fmt.Println(lessonType)
+
+
+	word1, word2, _ := "hello", "world", "1"
+
+	fmt.Println(word1, word2)
 }
--- lectures/exercise/variables/variables.go
+++ lectures/exercise/variables/variables.go
@@ -25,4 +25,33 @@ package main
 import "fmt"
 
 func main() {
+	//Requirements:
+//* Store your favorite color in a variable using the `var` keyword
+     var myFavColor = "black"
+	 fmt.Println("my fav color=", myFavColor)
+
+//* Store your birth year and age (in years) in two variables using
+//  compound assignment
+    year, age := 1993, 29
+	fmt.Println("my birth year=", year, "my age=",age)
+    
+//* Store your first & last initials in two variables using block assignment
+       var(
+		 firstInitial = "J"
+		 lastInitial = "K"
+	   )
+       
+	   fmt.Println(firstInitial, lastInitial)
+
+//* Declare (but don't assign!) a variable for your age (in days),
+//  then assign it on the next line by multiplying 365 with the age
+// 	variable created earlier
+
+    var ageInDays int
+
+    ageInDays = 365 * age
+    
+	fmt.Println(ageInDays)
+
+
 }
