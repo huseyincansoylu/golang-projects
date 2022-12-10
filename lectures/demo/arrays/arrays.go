4d3838fa94765f8db20bb79667535175f7a0ca9a
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-10 17:38:58 +0300

    arrays in go

--- lectures/demo/arrays/arrays.go
+++ lectures/demo/arrays/arrays.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
-func main() {
+type Room struct {
+	name string
+	cleaned bool
+}
 
+func check(rooms [4]Room) {
+    for i := 0; i < len(rooms); i++ {
+         room := rooms[i]
+		 if room.cleaned {
+            fmt.Println(room.name, "is clean")
+		 } else {
+			fmt.Println(room.name, "is not clean")
+		 }
+	}
+} 
+
+func main() {
+    rooms := [...]Room{
+		{name:  "salon", cleaned: true},
+		{name:  "mutfak", cleaned: false},
+		{name:  "banyo", cleaned: false},
+		{name:  "yatak odasi", cleaned: true},
+	}
+
+	check(rooms) 
 }
--- lectures/exercise/arrays/arrays.go
+++ lectures/exercise/arrays/arrays.go
@@ -18,6 +18,39 @@ package main
 
 import "fmt"
 
-func main() {
+type Product struct {
+	 price int
+	 name string
+}
 
+func printStats(list [4]Product) {
+    var cost , totalItems int
+
+	 for i := 0; i < len(list); i++ {
+		item := list[i]
+
+		cost += item.price
+
+		if(item.name != "") {
+			totalItems += 1
+		}
+	 }
+
+	 fmt.Println("Last item in the list", list[totalItems - 1])
+	 fmt.Println("total items",  totalItems)
+	 fmt.Println("total cost ", cost)
+}
+
+func main() {
+     shoppingList :=  [4]Product{
+		{10, "Muz"},
+		{5, "Elma"},
+		{3, "limon"},
+	 }
+
+	 printStats(shoppingList)
+
+	 shoppingList[3] = Product{100, "antep fistigi"}
+
+	 printStats(shoppingList)
 }
