37b6da5ed793ac4e68898ba2de789ab9d6aad9b0
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-10 20:59:38 +0300

    maps in go

--- lectures/demo/maps/maps.go
+++ lectures/demo/maps/maps.go
@@ -2,5 +2,39 @@ package main
 
 import "fmt"
 
+
+
 func main() {
+    shoppingList := make(map[string]int)
+
+	shoppingList["eggs"] = 11
+	shoppingList["milk"] = 5
+	shoppingList["bread"] += 1 
+
+	shoppingList["eggs"] += 9
+
+	fmt.Println(shoppingList)
+
+	delete(shoppingList, "milk")
+
+	fmt.Println(shoppingList) 
+
+	cereal, found := shoppingList["cereal"]
+
+	if !found {
+		fmt.Println(("no cereal"))
+	} else {
+		fmt.Println(cereal)
+	}
+  
+	totalItems := 0
+
+	for _, amount := range shoppingList {
+		totalItems += amount
+	}
+
+	fmt.Println(totalItems)
+	 
 }
+
+
