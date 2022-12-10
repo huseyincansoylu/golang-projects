fe301ed3050936fee68dfcf84d6882e649c72061
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-10 18:53:48 +0300

    ranges in go

--- lectures/demo/ranges/ranges.go
+++ lectures/demo/ranges/ranges.go
@@ -3,4 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	slice := []string{"hello", "world", "!"}
+
+	 for i, element := range slice{
+       fmt.Println(i, element)
+	   for _, letter := range element {
+		fmt.Printf("char %q\n", letter)
+	   }
+	 }
 }
