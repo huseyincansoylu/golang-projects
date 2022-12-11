a917d19f1db026f65f7b2850311e431189ceaa83
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-11 18:09:16 +0300

    variadics in go

--- lectures/demo/variadics/variadics.go
+++ lectures/demo/variadics/variadics.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) int {
+    sum  := 0
+
+	for _ ,num := range nums {
+		sum += num
+	}
+
+	return sum
+}
+
+
 func main() {
+    
+	a := []int{1,2,3}
+	b := []int{4,5,6}
+	
+	all := append(a , b...) 
+
+	answer := sum(all...)
+
+	fmt.Println(answer)
 
 }
