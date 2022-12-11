07338a9988217c0cd56b472dbc75f75fbb5efa87
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-11 19:46:08 +0300

    interface in go

--- lectures/demo/interfaces/interfaces.go
+++ lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
-func main() {
+type Preparer interface {
+     PrepareDish()
+}
 
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+
+func (s Salad) PrepareDish() {
+	fmt.Println("chop  salad")
+	fmt.Println("add  dressing ")
+}
+
+func prepareDishes(dishes []Preparer ) {
+	 fmt.Println("prepare dishes ")
+	 for i := 0; i < len(dishes); i++ {
+		  dish := dishes[i]  
+		  fmt.Println(dish)
+		  dish.PrepareDish() 
+	 }
+	 fmt.Println( )
+}
+   
+func main() {
+     dishes := []Preparer{Chicken("Chicken"), Salad("saladdd ")}
+
+	 prepareDishes(dishes)
 }
