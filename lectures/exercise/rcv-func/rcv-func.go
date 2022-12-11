ef8a395657cbe75366030809c0c91a41fd61b706
hcsoylu <hcsoylu.dev@gmail.com>
2022-12-11 17:01:11 +0300

    receiver function in go

--- lectures/exercise/rcv-func/rcv-func.go
+++ lectures/exercise/rcv-func/rcv-func.go
@@ -15,7 +15,70 @@
 package main
 
 import "fmt"
+ 
+type Player struct {
+	name string
+	health , maxHealth uint
+	energy , maxEnergy uint 
+}
+
+
+func (player *Player) addHealth(amount uint) {
+    player.health +=amount
+	 
+	if player.health > player.maxHealth {
+		player.health =  player.maxHealth
+	}
+
+	fmt.Println(player)
+}
+
+
+func (player *Player) applyDamage(amount uint) {
+    
+	if player.health - amount > player.health {
+		player.health = 0 
+	} else {
+		player.health -= amount 
+	}
+
+	fmt.Println(player)
+}
+
+func (player *Player) applyEnergy(amount uint) {
+    
+	player.energy += amount
+
+	if player.energy  > player.maxEnergy  {
+		player.energy = player.maxEnergy  
+	}
+
+	fmt.Println(player)
+}
+
+func (player *Player) consumeEnergy(amount uint) {
+    
+	if player.energy - amount > player.energy {
+		player.energy = 0 
+	} else {
+		player.energy  -= amount 
+	}
+
+	fmt.Println(player)
+}
+
 
 func main() {
+    player :=  Player{
+		name: "knight",
+		 health: 100,
+		 maxHealth: 100,
+		 energy: 500,
+		 maxEnergy: 500,
+	}
 
+	player.applyDamage(40)
+	player.addHealth(10)
+	player.consumeEnergy(20)
+	player.applyEnergy(10)
 }
